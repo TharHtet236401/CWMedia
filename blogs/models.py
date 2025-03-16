db911f54d1c4be2d7f72b895c43c6601b93380d9
TharHtet236401 <tharhteta3@gmail.com>
2025-03-16 13:03:31 +0700

    added the nav bar

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title + " by " + (self.author.username if self.author else "Unknown")
+
+
--- blogs/views.py
+++ blogs/views.py
@@ -1,15 +1,11 @@
 from django.shortcuts import render
 from django.conf import settings
 import os
-
+from .models import Blog
 
 # Create your views here.
 def home(request):
     # Debug prints
-    base_template_path = os.path.join(settings.BASE_DIR, 'templates', 'base.html')
-    print(f"Looking for base.html at: {base_template_path}")
-    print(f"File exists: {os.path.exists(base_template_path)}")
-    print(f"Template dirs: {settings.TEMPLATES[0]['DIRS']}")
-    
-    return render(request, 'blogs/home.html')
+    blogs = Blog.objects.all()
+    return render(request, 'blogs/home.html', {'blogs': blogs})
 
