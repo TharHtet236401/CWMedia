c5d02691d1f0fe9a72a6efdc0b4d2ae326ec0d19
TharHtet236401 <tharhteta3@gmail.com>
2025-03-16 13:20:19 +0700

    added the error message

--- blogs/views.py
+++ blogs/views.py
@@ -2,13 +2,24 @@ from django.shortcuts import render
 from django.conf import settings
 import os
 from .models import Blog
-
+from django.contrib import messages
 # Create your views here.
 def home(request):
-    # Debug prints
-    blogs = Blog.objects.all()
+    try:
+        blogs = Blog.objects.all()
+        context = {
+            'blogs': blogs
+        }
+        messages.success(request, "Blogs fetched successfully")
+        return render(request, 'blogs/home.html', context)
+    except Exception as e:
+        messages.error(request, f"Error: {e}")
+        return redirect('home')
+
+def single_blog_view(request, pk):
+    blog = Blog.objects.get(pk=pk)
     context = {
-        'blogs': blogs
+        'blog': blog
     }
-    return render(request, 'blogs/home.html', context)
+    return render(request, 'blogs/single_blog.html', context)
 
