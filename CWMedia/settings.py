ceed128aecb7f59c7b6afa1864efa246b07a1286
TharHtet236401 <tharhteta3@gmail.com>
2025-03-16 17:48:46 +0700

    Added custom message tags for Django messages in settings and updated alert classes in templates for better styling. Cleaned up commented code in navigation template.

--- CWMedia/settings.py
+++ CWMedia/settings.py
@@ -138,3 +138,12 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
